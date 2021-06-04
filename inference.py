5bf5d89deba2ef8359b062d980c97ba984245a26
unaG <veraguo11@163.com>
2021-06-04 12:03:16 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -91,10 +91,9 @@ class InferenceDataset(Dataset):
             end_index = c_count + self.win_size // 2
             if start_index<0:
                 start_index=0
-                pad_f_mfcc = np.repeat(f_mfcc_all, repeats=[self.win_size-end_index]+[1]*(f_mfcc_all.shape[1]-1), axis=1)
-                audio_feat = pad_f_mfcc[:, start_index: end_index].transpose(1, 0)
-                # audio_feat = np.hstack((pad_f_mfcc, audio_feat))
-                print(f"pad_f_mfcc:{pad_f_mfcc.shape}, audio_feat: {audio_feat.shape}")
+                repeat_pattern = [self.win_size-end_index]+[1]*(f_mfcc_all.shape[1]-1)
+                pad_f_mfcc_all = np.repeat(f_mfcc_all, repeats=repeat_pattern, axis=1) # Padding with the first column
+                audio_feat = pad_f_mfcc_all[:, start_index: end_index].transpose(1, 0)
             else:
                 audio_feat = f_mfcc_all[:, start_index: end_index].transpose(1, 0)
             audio_features.append(torch.from_numpy(audio_feat).unsqueeze(dim=0)) #.unsqueeze(dim=0)
