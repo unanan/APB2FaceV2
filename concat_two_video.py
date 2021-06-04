42128ec2d35d4fe7c1915a160ea360debaf6e59c
unaG <veraguo11@163.com>
2021-06-04 14:14:04 +0800

    Update concat_two_video.py

--- concat_two_video.py
+++ concat_two_video.py
@@ -2,6 +2,7 @@ import cv2
 import argparse
 from tqdm import tqdm
 import numpy as np
+import os
 
 def get_options():
     parser = argparse.ArgumentParser(description="Get specific options information in a command")
@@ -45,7 +46,17 @@ def main():
         concat_frames.append(np.concatenate([left_frame, right_frame], axis=1))
         ret_left, left_frame = cap_left.read()
         ret_right, right_frame = cap_right.read()
-    write_video_frames(concat_frames, opt.output_video_path)
+    write_video_frames(concat_frames, "tmp.mp4")
+
+    # Extract audio from left video #TODO
+    audio_path = os.path.splitext(opt.left_video_path)[0]+".wav"
+    command = f"ffmpeg -i {opt.left_video_path} -vn -acodec copy {audio_path}"
+    os.system(command)
+
+    # Write audio to concatenated video
+    command = 'ffmpeg -y -i {} -i {} -strict -2 -q:v 1 {}'.format(audio_path, "tmp.mp4", opt.output_video_path)
+    os.system(command)
+    print(f"Result video write to: {opt.output_video_path}")
 
 
 if __name__ == '__main__':
